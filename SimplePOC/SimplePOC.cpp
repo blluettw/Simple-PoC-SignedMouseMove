33c0767a3b7e61dad6a7b036b5bc59b298f6d794
ILLUSION <76027489+GLX-ILLUSION@users.noreply.github.com>
2024-01-05 03:12:25 -0300

    Update SimplePOC.cpp

--- SimplePOC/SimplePOC.cpp
+++ SimplePOC/SimplePOC.cpp
@@ -11,7 +11,7 @@ int main()
     driver.initdriver();
   
 
-    /// u can use buttons to click example 0x1 left click , but u need release button with 0x2 
+    /// u can use buttons to click example 0x1 left click , but u need release button with 0x2 in last parametter
     driver.mouse_event(222, 222,0);
     Sleep(1000);
     driver.mouse_event(-222, -222,0);
